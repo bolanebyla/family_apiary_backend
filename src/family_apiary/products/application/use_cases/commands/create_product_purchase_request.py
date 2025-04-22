7c4cdee674aab6a8acf0eb3b48cbbd71980e0105
Алексей Лесков <alexleskov2000@gmail.com>
2025-04-22 10:24:38 +0800

    CreateProductPurchaseRequestCommand

--- src/family_apiary/products/application/use_cases/commands/create_product_purchase_request.py
+++ src/family_apiary/products/application/use_cases/commands/create_product_purchase_request.py
@@ -5,12 +5,20 @@ from commons.cqrs.base import CommandHandler
 
 @dataclass
 class CreateProductPurchaseRequestCommand:
+    """
+    Команда на создание заявку на покупку продукции
+    """
+
     pass
 
 
 class CreateProductPurchaseRequestHandler(
     CommandHandler[CreateProductPurchaseRequestCommand, None]
 ):
+    """
+    Создание заявку на покупку продукции
+    """
+
     async def handle(
         self, command: CreateProductPurchaseRequestCommand
     ) -> None:
