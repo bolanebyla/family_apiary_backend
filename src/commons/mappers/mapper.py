f17fa13b9eb1a68a6f65222c29caf40d0e58a745
Алексей Лесков <alexleskov2000@gmail.com>
2025-05-15 22:05:56 +0800

    Mapper

---
+++ src/commons/mappers/mapper.py
@@ -0,0 +1,37 @@
+from abc import abstractmethod
+from typing import Any, Callable, Generic, Iterable, Protocol, Type, TypeVar
+
+T = TypeVar('T')
+R = TypeVar('R')
+C = TypeVar('C', bound='MapperConfig')
+
+
+class MapperConfig(Protocol[T, R]):
+    source_type: Type[T]
+    target_type: Type[R]
+
+    field_mappings: dict[str, str] | None = None
+    """Словарь соответствия полей (исходное_поле -> целевое_поле)"""
+
+    computed_fields: dict[str, Callable[[Any], Any]] | None = None
+    """Словарь вычисляемых полей (поле -> функция_вычисления)"""
+
+    nested_mappers: list['MapperConfig[Type, Type]'] | None = None
+    """Словарь вложенных мапперов для сложных полей (тип поля -> маппер)"""
+
+    def __init__(self):
+        self.field_mappings = self.field_mappings or {}
+        self.computed_fields = self.computed_fields or {}
+        self.nested_mappers = self.nested_mappers or []
+
+
+class Mapper(Generic[C, T, R]):
+    config: C
+
+    @abstractmethod
+    def map(self, source: T, extra: dict[str, Any] = None) -> R: ...
+
+    @abstractmethod
+    def map_many(
+        self, sources: Iterable[T], extra: dict[str, Any] = None
+    ) -> Iterable[R]: ...
---
+++ src/commons/mappers/mapper_impl.py
@@ -0,0 +1,74 @@
+from dataclasses import dataclass
+from typing import Any, Iterable, Type
+
+from dataclass_mapper import create_mapper, map_to
+
+from commons.mappers.mapper import C, Mapper, MapperConfig, R, T
+
+
+class MapperImpl(Mapper[C, T, R]):
+    def __init__(self, mapper_config: MapperConfig[T, R]):
+        self.mapper_config = mapper_config
+
+        for nested_mapper_config in mapper_config.nested_mappers:
+            self._init_mapper(mapper_config=nested_mapper_config)
+
+        self._init_mapper(mapper_config=mapper_config)
+
+    @staticmethod
+    def _init_mapper(mapper_config: MapperConfig[Type, Type]):
+        fields_mapping = (
+            mapper_config.field_mappings | mapper_config.computed_fields
+        )
+        create_mapper(
+            SourceCls=mapper_config.source_type,
+            TargetCls=mapper_config.target_type,
+            mapping=fields_mapping,
+        )
+
+    def map(
+        self,
+        source: T,
+        extra: dict[str, Any] = None,
+    ) -> R:
+        return map_to(
+            obj=source,
+            target=self.mapper_config.target_type,
+            extra=extra,
+        )
+
+    def map_many(
+        self,
+        sources: Iterable[T],
+        extra: dict[str, Any] = None,
+    ) -> list[R]:
+        return [self.map(source, extra) for source in sources]
+
+
+if __name__ == '__main__':
+
+    @dataclass
+    class Person:
+        id: int
+        name: str
+
+    @dataclass
+    class Contact:
+        id: int
+        title: str
+        slug: str
+
+    class ContactMapperConfig(MapperConfig[Person, Contact]):
+        source_type = Person
+        target_type = Contact
+
+        field_mappings = {'title': 'name'}
+        computed_fields = {'slug': lambda source: f'{source.id}_{source.name}'}
+
+    contact_mapper_config = ContactMapperConfig()
+    contact_mapper = MapperImpl(mapper_config=contact_mapper_config)
+
+    person = Person(id=1, name='test_name')
+
+    contact = contact_mapper.map(person)
+    print(contact)
