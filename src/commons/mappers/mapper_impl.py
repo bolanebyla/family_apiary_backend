de848b81f715390d74d8102d7a79415d2c02c267
Алексей Лесков <alexleskov2000@gmail.com>
2025-06-19 18:22:31 +0800

    Class mapper

--- src/commons/mappers/mapper_impl.py
+++ src/commons/mappers/mapper_impl.py
@@ -1,8 +1,9 @@
 from dataclasses import fields, is_dataclass
+from enum import Enum
 from inspect import isclass
 from typing import Any, Iterable, Type, get_type_hints
 
-from pydantic import BaseModel, Field, ValidationError, create_model
+from pydantic import BaseModel, ValidationError
 
 from commons.mappers.mapper import (
     C,
@@ -13,192 +14,220 @@ from commons.mappers.mapper import (
 )
 
 
-class MapperImpl(Mapper):
-    """
-    Реализация маппера
+class ObjectType(Enum):
+    """Типы объектов, поддерживаемые маппером"""
 
-    Поддерживает мапинг датаклассав и pydantic моделей.
-    Для маппинга используется Pydantic.
-    """
+    DATACLASS = 'dataclass'
+    PYDANTIC = 'pydantic'
+    DICT_LIKE = 'dict_like'
+    UNSUPPORTED = 'unsupported'
 
-    def __init__(self):
-        # Кэш для вложенных мапперов
-        self._nested_mappers = {}
 
-    def _init_nested_mapper_configs(
-        self, nested_mapper_configs: list[MapperConfig[Any, Any]]
-    ) -> None:
-        """Инициализация вложенных мапперов"""
-        for nested_mapper_config in nested_mapper_configs:
-            if nested_mapper_config.source_type not in self._nested_mappers:
-                self._nested_mappers[nested_mapper_config.source_type] = (
-                    MapperImpl()
-                )
+class ObjectTypeDetector:
+    """Детектор типов объектов"""
 
-    def _create_mapping_model(
-        self, mapper_config: MapperConfig[Type, Type]
-    ) -> Type[BaseModel]:
-        """Создание динамической модели Pydantic для маппинга"""
-        fields_dict = {}
+    @staticmethod
+    def detect_type(obj: Any) -> ObjectType:
+        """Определяет тип объекта"""
+        if is_dataclass(obj):
+            return ObjectType.DATACLASS
+        elif isinstance(obj, BaseModel):
+            return ObjectType.PYDANTIC
+        elif hasattr(obj, '__dict__'):
+            return ObjectType.DICT_LIKE
+        else:
+            return ObjectType.UNSUPPORTED
 
-        # Добавляем поля для маппинга
-        for target_field, source_field in mapper_config.field_mappings.items():
-            field_type = self._get_field_type(
-                source_field, mapper_config.source_type
-            )
-            fields_dict[target_field] = (field_type, Field(alias=source_field))
-
-        # Добавляем вычисляемые поля
-        for (
-            computed_field_name,
-            computed_func,
-        ) in mapper_config.computed_fields.items():
-            # Для вычисляемых полей используем Any, так как они будут вычисляться динамически
-            fields_dict[computed_field_name] = (Any, Field(default=None))
-
-        # Добавляем поля из extra
-        extra_field_names = self._get_extra_field_names(mapper_config)
-        for extra_field_name in extra_field_names:
-            fields_dict[extra_field_name] = (Any, Field(default=None))
-
-        # Создаем модель
-        model_name = f'{mapper_config.source_type.__name__}To{mapper_config.target_type.__name__}Mapper'
-        return create_model(model_name, **fields_dict)
-
-    def _get_field_type(self, field_name: str, cls: Type[Any]) -> Type[Any]:
-        """Получение типа поля"""
+    @staticmethod
+    def detect_class_type(cls: Type[Any]) -> ObjectType:
+        """Определяет тип класса"""
         if is_dataclass(cls):
+            return ObjectType.DATACLASS
+        elif isclass(cls) and issubclass(cls, BaseModel):
+            return ObjectType.PYDANTIC
+        elif hasattr(cls, '__dict__'):
+            return ObjectType.DICT_LIKE
+        else:
+            return ObjectType.UNSUPPORTED
+
+
+class ObjectConverter:
+    """Универсальный конвертер объектов в словари"""
+
+    def __init__(self, type_detector: ObjectTypeDetector):
+        self._type_detector = type_detector
+
+    def to_dict(self, obj: Any) -> dict[str, Any]:
+        """Конвертирует объект в словарь"""
+        obj_type = self._type_detector.detect_type(obj)
+
+        if obj_type == ObjectType.DATACLASS:
+            return {
+                field.name: getattr(obj, field.name) for field in fields(obj)
+            }
+        elif obj_type == ObjectType.PYDANTIC:
+            return obj.dict()
+        elif obj_type == ObjectType.DICT_LIKE:
+            return obj.__dict__
+        else:
+            raise TypeError(
+                f'Cannot convert object of type {type(obj)} to dict'
+            )
+
+
+class FieldTypeResolver:
+    """Резолвер типов полей"""
+
+    def __init__(self, type_detector: ObjectTypeDetector):
+        self._type_detector = type_detector
+
+    def get_field_type(self, field_name: str, cls: Type[Any]) -> Type[Any]:
+        """Получает тип поля"""
+        cls_type = self._type_detector.detect_class_type(cls)
+
+        if cls_type == ObjectType.DATACLASS:
             field_info = next(
                 (f for f in fields(cls) if f.name == field_name), None
             )
             if field_info:
                 return field_info.type
-        elif isclass(cls) and issubclass(cls, BaseModel):
+        elif cls_type == ObjectType.PYDANTIC:
             annotations = get_type_hints(cls)
             return annotations.get(field_name, Any)
 
         return Any
 
-    def _get_extra_field_names(
-        self, mapper_config: MapperConfig[Type, Type]
-    ) -> list[str]:
-        """Получение имен полей, которые должны быть загружены из extra"""
-        target_fields = self._get_fields(mapper_config.target_type)
-        source_fields = self._get_fields(mapper_config.source_type)
-        mapped_fields = set(mapper_config.field_mappings.keys())
-        computed_fields = set(mapper_config.computed_fields.keys())
 
-        return list(
-            set(target_fields)
-            - set(source_fields)
-            - mapped_fields
-            - computed_fields
-        )
+class FieldExtractor:
+    """Извлекатель полей из классов"""
 
-    @staticmethod
-    def _get_fields(cls: Type[Any]) -> list[str]:
-        """Получение списка полей класса"""
-        if is_dataclass(cls):
-            # Получаем все поля, включая наследуемые
-            all_fields = []
-            current_cls = cls
+    def __init__(self, type_detector: ObjectTypeDetector):
+        self._type_detector = type_detector
 
-            # Проходим по иерархии наследования
-            while current_cls is not None:
-                if is_dataclass(current_cls):
-                    all_fields.extend([f.name for f in fields(current_cls)])
-                current_cls = (
-                    current_cls.__bases__[0] if current_cls.__bases__ else None
-                )
+    def get_fields(self, cls: Type[Any]) -> list[str]:
+        """Получает список полей класса"""
+        cls_type = self._type_detector.detect_class_type(cls)
 
-                # Останавливаемся на object
-                if current_cls is object:
-                    break
-
-            return list(
-                dict.fromkeys(all_fields)
-            )  # Убираем дубликаты, сохраняя порядок
-        elif isclass(cls) and issubclass(cls, BaseModel):
+        if cls_type == ObjectType.DATACLASS:
+            return self._get_dataclass_fields(cls)
+        elif cls_type == ObjectType.PYDANTIC:
             return list(cls.__annotations__.keys())
         else:
             raise TypeError(f'The mapper does not work with the "{cls}" type')
 
-    def map(
-        self,
-        source: T,
-        mapper_config: C,
-        extra: dict[str, Any] | None = None,
-    ) -> R:
-        """Маппинг одного объекта"""
-        try:
-            # Инициализируем вложенные мапперы
-            self._init_nested_mapper_configs(
-                nested_mapper_configs=mapper_config.nested_mapper_configs,
+    @staticmethod
+    def _get_dataclass_fields(cls: Type[Any]) -> list[str]:
+        """Получает поля dataclass с учетом наследования"""
+        all_fields = []
+        current_cls = cls
+
+        while current_cls is not None and current_cls is not object:
+            if is_dataclass(current_cls):
+                all_fields.extend([f.name for f in fields(current_cls)])
+            current_cls = (
+                current_cls.__bases__[0] if current_cls.__bases__ else None
             )
 
-            # Преобразуем объект в словарь
-            source_dict = self._object_to_dict(source)
+        return list(
+            dict.fromkeys(all_fields)
+        )  # Убираем дубликаты, сохраняя порядок
 
-            # Получаем поля целевого типа
-            target_fields = self._get_fields(mapper_config.target_type)
 
-            # Применяем маппинг полей
-            mapped_dict = {}
+class FieldMapper:
+    """Маппер полей"""
 
-            # Копируем поля с переименованием
-            for (
-                target_field,
-                source_field,
-            ) in mapper_config.field_mappings.items():
-                if (
-                    source_field in source_dict
-                    and target_field in target_fields
-                ):
-                    mapped_dict[target_field] = source_dict[source_field]
+    def __init__(self, field_extractor: FieldExtractor):
+        self._field_extractor = field_extractor
 
-            # Копируем остальные поля, которые есть в целевом типе
-            for field_name, value in source_dict.items():
-                if (
-                    field_name not in mapper_config.field_mappings.values()
-                    and field_name in target_fields
-                ):
-                    mapped_dict[field_name] = value
+    def map_fields(
+        self,
+        source_dict: dict[str, Any],
+        mapper_config: MapperConfig[Type, Type],
+        target_fields: list[str],
+    ) -> dict[str, Any]:
+        """Маппит поля из исходного объекта в целевой"""
+        mapped_dict = {}
 
-            # Применяем вычисляемые поля - передаем исходный объект
-            for (
-                computed_field_name,
-                computed_func,
-            ) in mapper_config.computed_fields.items():
-                if computed_field_name in target_fields:
-                    mapped_dict[computed_field_name] = computed_func(source)
+        # Копируем поля с переименованием
+        for target_field, source_field in mapper_config.field_mappings.items():
+            if source_field in source_dict and target_field in target_fields:
+                mapped_dict[target_field] = source_dict[source_field]
 
-            # Добавляем поля из extra, которые есть в целевом типе
-            if extra:
-                for field_name, value in extra.items():
-                    if field_name in target_fields:
-                        mapped_dict[field_name] = value
+        # Копируем остальные поля, которые есть в целевом типе
+        for field_name, value in source_dict.items():
+            if (
+                field_name not in mapper_config.field_mappings.values()
+                and field_name in target_fields
+            ):
+                mapped_dict[field_name] = value
 
-            # Обрабатываем вложенные объекты
-            self._process_nested_objects(mapped_dict, mapper_config, extra)
+        return mapped_dict
 
-            # Создаем целевой объект
-            return mapper_config.target_type(**mapped_dict)
 
-        except ValidationError as e:
-            raise ValueError(f'Validation error during mapping: {e}')
-        except Exception as e:
-            raise RuntimeError(f'Mapping error: {e}')
+class ComputedFieldProcessor:
+    """Обработчик вычисляемых полей"""
 
-    def _process_nested_objects(
+    def process_computed_fields(
+        self,
+        source: Any,
+        mapper_config: MapperConfig[Type, Type],
+        target_fields: list[str],
+    ) -> dict[str, Any]:
+        """Обрабатывает вычисляемые поля"""
+        computed_dict = {}
+
+        for (
+            computed_field_name,
+            computed_func,
+        ) in mapper_config.computed_fields.items():
+            if computed_field_name in target_fields:
+                computed_dict[computed_field_name] = computed_func(source)
+
+        return computed_dict
+
+
+class ExtraFieldProcessor:
+    """Обработчик дополнительных полей"""
+
+    def __init__(self, field_extractor: FieldExtractor):
+        self._field_extractor = field_extractor
+
+    def process_extra_fields(
+        self,
+        extra: dict[str, Any] | None,
+        mapper_config: MapperConfig[Type, Type],
+    ) -> dict[str, Any]:
+        """Обрабатывает дополнительные поля"""
+        if not extra:
+            return {}
+
+        target_fields = self._field_extractor.get_fields(
+            mapper_config.target_type
+        )
+        extra_dict = {}
+
+        for field_name, value in extra.items():
+            if field_name in target_fields:
+                extra_dict[field_name] = value
+
+        return extra_dict
+
+
+class NestedObjectProcessor:
+    """Обработчик вложенных объектов"""
+
+    def __init__(self, mapper: 'MapperImpl'):
+        self._mapper = mapper
+
+    def process_nested_objects(
         self,
         mapped_dict: dict[str, Any],
-        mapper_config: C,
+        mapper_config: MapperConfig[Type, Type],
         extra: dict[str, Any] | None = None,
     ) -> None:
-        """Обработка вложенных объектов"""
+        """Обрабатывает вложенные объекты"""
         for field_name, value in list(mapped_dict.items()):
             if isinstance(value, list):
-                # Обрабатываем списки
                 mapped_dict[field_name] = [
                     self._map_nested_object(
                         item, field_name, mapper_config, extra
@@ -206,7 +235,6 @@ class MapperImpl(Mapper):
                     for item in value
                 ]
             else:
-                # Обрабатываем одиночные объекты
                 mapped_value = self._map_nested_object(
                     value, field_name, mapper_config, extra
                 )
@@ -217,16 +245,15 @@ class MapperImpl(Mapper):
         self,
         obj: Any,
         field_name: str = None,
-        mapper_config: C = None,
+        mapper_config: MapperConfig[Type, Type] = None,
         extra: dict[str, Any] | None = None,
     ) -> Any:
-        """Маппинг вложенного объекта"""
+        """Маппит вложенный объект"""
         if obj is None:
             return None
 
         obj_type = type(obj)
-        if obj_type in self._nested_mappers:
-            # Находим соответствующий конфиг для вложенного объекта
+        if obj_type in self._mapper._nested_mappers:
             nested_config = next(
                 (
                     config
@@ -237,11 +264,119 @@ class MapperImpl(Mapper):
             )
 
             if nested_config:
-                return self._nested_mappers[obj_type].map(
+                return self._mapper._nested_mappers[obj_type].map(
                     obj, nested_config, extra=extra
                 )
 
         return obj
+
+
+class NestedMapperInitializer:
+    """Инициализатор вложенных мапперов"""
+
+    def initialize_nested_mappers(
+        self,
+        nested_mapper_configs: list[MapperConfig[Any, Any]],
+        nested_mappers: dict[Type, 'MapperImpl'],
+    ) -> None:
+        """Инициализирует вложенные мапперы"""
+        for nested_mapper_config in nested_mapper_configs:
+            if nested_mapper_config.source_type not in nested_mappers:
+                nested_mappers[nested_mapper_config.source_type] = MapperImpl()
+
+
+class ObjectValidator:
+    """Валидатор объектов"""
+
+    @staticmethod
+    def validate_mapping_result(
+        mapped_dict: dict[str, Any], target_type: Type[Any]
+    ) -> None:
+        """Валидирует результат маппинга"""
+        try:
+            target_type(**mapped_dict)
+        except ValidationError as e:
+            raise ValueError(f'Validation error during mapping: {e}')
+        except Exception as e:
+            raise RuntimeError(f'Mapping error: {e}')
+
+
+class MapperImpl(Mapper):
+    """
+    Реализация маппера
+
+    Поддерживает мапинг датаклассов и pydantic моделей.
+    Для маппинга используется Pydantic.
+    """
+
+    def __init__(self):
+        # Кэш для вложенных мапперов
+        self._nested_mappers = {}
+
+        # Инициализация зависимостей
+        self._type_detector = ObjectTypeDetector()
+        self._converter = ObjectConverter(self._type_detector)
+        self._field_extractor = FieldExtractor(self._type_detector)
+        self._field_mapper = FieldMapper(self._field_extractor)
+        self._computed_field_processor = ComputedFieldProcessor()
+        self._extra_field_processor = ExtraFieldProcessor(self._field_extractor)
+        self._nested_object_processor = NestedObjectProcessor(self)
+        self._nested_mapper_initializer = NestedMapperInitializer()
+        self._object_validator = ObjectValidator()
+
+    def map(
+        self,
+        source: T,
+        mapper_config: C,
+        extra: dict[str, Any] | None = None,
+    ) -> R:
+        """Маппинг одного объекта"""
+        try:
+            # Инициализируем вложенные мапперы
+            self._nested_mapper_initializer.initialize_nested_mappers(
+                nested_mapper_configs=mapper_config.nested_mapper_configs,
+                nested_mappers=self._nested_mappers,
+            )
+
+            # Преобразуем объект в словарь
+            source_dict = self._converter.to_dict(source)
+
+            # Получаем поля целевого типа
+            target_fields = self._field_extractor.get_fields(
+                mapper_config.target_type
+            )
+
+            # Применяем маппинг полей
+            mapped_dict = self._field_mapper.map_fields(
+                source_dict, mapper_config, target_fields
+            )
+
+            # Применяем вычисляемые поля
+            computed_dict = (
+                self._computed_field_processor.process_computed_fields(
+                    source, mapper_config, target_fields
+                )
+            )
+            mapped_dict.update(computed_dict)
+
+            # Добавляем поля из extra
+            extra_dict = self._extra_field_processor.process_extra_fields(
+                extra, mapper_config
+            )
+            mapped_dict.update(extra_dict)
+
+            # Обрабатываем вложенные объекты
+            self._nested_object_processor.process_nested_objects(
+                mapped_dict, mapper_config, extra
+            )
+
+            # Создаем целевой объект
+            return mapper_config.target_type(**mapped_dict)
+
+        except Exception as e:
+            if isinstance(e, (ValueError, RuntimeError)):
+                raise
+            raise RuntimeError(f'Mapping error: {e}')
 
     def map_many(
         self,
@@ -251,21 +386,6 @@ class MapperImpl(Mapper):
     ) -> list[R]:
         """Маппинг множества объектов"""
         return [self.map(source, mapper_config, extra) for source in sources]
-
-    def _object_to_dict(self, obj: Any) -> dict[str, Any]:
-        """Преобразование объекта в словарь"""
-        if is_dataclass(obj):
-            return {
-                field.name: getattr(obj, field.name) for field in fields(obj)
-            }
-        elif isinstance(obj, BaseModel):
-            return obj.dict()
-        elif hasattr(obj, '__dict__'):
-            return obj.__dict__
-        else:
-            raise TypeError(
-                f'Cannot convert object of type {type(obj)} to dict'
-            )
 
 
 if __name__ == '__main__':
